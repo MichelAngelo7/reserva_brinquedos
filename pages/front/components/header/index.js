ab1afa8076c260bb121d32484bbc3d2f34da8e42
michelangelo7 <m_che_l@hotmail.com>
2025-01-25 13:35:47 -0300

    Added css in header

--- pages/front/components/header/index.js
+++ pages/front/components/header/index.js
@@ -1,12 +1,27 @@
+import React from "react";
+import styles from "./Header.module.css";
+
 export default function Header() {
   return (
-    <div>
-      <a href="/">Home</a>
-      <a href="/front/events">Exibir Eventos</a>
-      <a href="/front/event">Cad. Evento</a>
-      <a href="/front/resources">Exibir Recursos</a>
-      <a href="/front/resource">Cad. Recursos</a>
-      <a href="/front/status">Status</a>
-    </div>
+    <nav className={styles.nav}>
+      <a className={styles.a} href="/">
+        Home
+      </a>
+      <a className={styles.a} href="/front/events">
+        Exibir Eventos
+      </a>
+      <a className={styles.a} href="/front/event">
+        Cad. Evento
+      </a>
+      <a className={styles.a} href="/front/resources">
+        Exibir Recursos
+      </a>
+      <a className={styles.a} href="/front/resource">
+        Cad. Recursos
+      </a>
+      <a className={styles.a} href="/front/status">
+        Status
+      </a>
+    </nav>
   );
 }
