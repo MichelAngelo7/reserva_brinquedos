3b48b2b3aee132e7e93d17b3bf1e49c8d07ed278
michelangelo7 <m_che_l@hotmail.com>
2025-01-25 09:06:22 -0300

    test fix 404 on vercel

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Home from "./front/home";
+import home from "./front/home";
 import { Container } from "./front/styles/GlobalStyles";
 import Header from "./front/components/header";
 
-function App() {
+function Home() {
   return (
     <Container>
       <Header />
-      <Home />
+      <home />
     </Container>
   );
 }
 
-export default App;
+export default Home;
