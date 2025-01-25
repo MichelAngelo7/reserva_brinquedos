c058140c9f28a2b74480b9887ea6163440abb497
michelangelo7 <m_che_l@hotmail.com>
2025-01-25 15:07:13 -0300

    Create migrations page

---
+++ pages/front/migrations/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from "../components/header";
+import { useState } from "react";
+import styles from "./Resource.module.css";
+
+export default function resource() {
+  const [id, setId] = useState(null);
+  const [name, setName] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: "POST",
+      body: JSON.stringify({}),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((responseBody) => {
+        if (responseBody) {
+          alert("cadastrado" + responseBody);
+        }
+      });
+  }
+
+  return (
+    <div>
+      <Header />
+      <h1>Rodar Migrations</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <button type="subimit">Enviar</button>
+      </form>
+    </div>
+  );
+}
