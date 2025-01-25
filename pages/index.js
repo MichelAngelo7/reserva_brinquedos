abeacf70756df25e690121b87124aa5ea5a45c18
michelangelo7 <m_che_l@hotmail.com>
2025-01-25 13:50:46 -0300

    Added ccs in home

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,45 @@
 import React from "react";
 import Header from "./front/components/header";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <a href="front/events">
-        <div>
-          <h3>Exibir Eventos Cadastrados</h3>
-          <h4>Verifique todos os eventos cadastrados no sistema</h4>
+        <div className={styles.card}>
+          <h3 className={styles.card__info}>Exibir Eventos Cadastrados</h3>
+          <h4 className={styles.card__info__h4}>
+            Verifique todos os eventos cadastrados no sistema
+          </h4>
         </div>
       </a>
       <a href="front/event">
-        <div>
-          <h3>Cadastro de Evento</h3>
-          <h4>Cadastre um novo Evento</h4>
+        <div className={styles.card}>
+          <h3 className={styles.card__info}>Cadastro de Evento</h3>
+          <h4 className={styles.card__info__h4}>Cadastre um novo Evento</h4>
         </div>
       </a>
       <a href="front/resources">
-        <div>
-          <h3>Exibir Brinquedos cadastrados</h3>
-          <h4>Verifique todos os Brinquedos cadastrados no sistema</h4>
+        <div className={styles.card}>
+          <h3 className={styles.card__info}>Exibir Brinquedos cadastrados</h3>
+          <h4 className={styles.card__info__h4}>
+            Verifique todos os Brinquedos cadastrados no sistema
+          </h4>
         </div>
       </a>
       <a href="front/resource">
-        <div>
-          <h3>Cadastro de Brinquedo</h3>
-          <h4>Cadastre um novo Brinquedo</h4>
+        <div className={styles.card}>
+          <h3 className={styles.card__info}>Cadastro de Brinquedo</h3>
+          <h4 className={styles.card__info__h4}>Cadastre um novo Brinquedo</h4>
         </div>
       </a>
       <a href="front/status">
-        <div>
-          <h3>Status</h3>
-          <h4> Verifique o status da aplicação</h4>
+        <div className={styles.card}>
+          <h3 className={styles.card__info}>Status</h3>
+          <h4 className={styles.card__info__h4}>
+            Verifique o status da aplicação
+          </h4>
         </div>
       </a>
     </div>
